e31a04eb1e9c8e52ecc8d5aee12b4bc47c9ff4ed
Francesco Rombecchi <rombecchi@gmail.com>
2018-04-10 11:01:32 +0200

    Update version-compare.js

--- version-compare.js
+++ version-compare.js
@@ -6,27 +6,16 @@
  * @param {string} v1 The first version to be compared.
  * @param {string} v2 The second version to be compared.
  * @param {object} [options] Optional flags that affect comparison behavior:
- * <ul>
- *     <li>
- *         <tt>lexicographical: true</tt> compares each part of the version strings lexicographically instead of
- *         naturally; this allows suffixes such as "b" or "dev" but will cause "1.10" to be considered smaller than
- *         "1.2".
- *     </li>
- *     <li>
- *         <tt>zeroExtend: true</tt> changes the result if one version string has less parts than the other. In
- *         this case the shorter string will be padded with "zero" parts instead of being considered smaller.
- *     </li>
- * </ul>
- * @returns {number|NaN}
- * <ul>
- *    <li>0 if the versions are equal</li>
- *    <li>a negative integer iff v1 < v2</li>
- *    <li>a positive integer iff v1 > v2</li>
- *    <li>NaN if either version string is in the wrong format</li>
- * </ul>
+ * lexicographical: (true/[false]) compares each part of the version strings lexicographically instead of naturally; 
+ *                  this allows suffixes such as "b" or "dev" but will cause "1.10" to be considered smaller than "1.2".
+ * zeroExtend: ([true]/false) changes the result if one version string has less parts than the other. In
+ *             this case the shorter string will be padded with "zero" parts instead of being considered smaller.
  *
- * @copyright by Jon Papaioannou (["john", "papaioannou"].join(".") + "@gmail.com")
- * @license This function is in the public domain. Do what you want with it, no strings attached.
+ * @returns {number|NaN}
+ * - 0 if the versions are equal
+ * - a negative integer iff v1 < v2
+ * - a positive integer iff v1 > v2
+ * - NaN if either version string is in the wrong format
  */
  
 function versionCompare(v1, v2, options) {
